884717d9ec7441de5108519fab6dcbccf97617f3
oungo <y2k6273@naver.com>
2022-08-23 23:08:40 +0900

    마우스 올렸을때 로드

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ const LazyImageModal = lazy(() => import('./components/ImageModal'))
 function App() {
     const [showModal, setShowModal] = useState(false)
 
+    const handleMouseEnter = () => {
+        const component = import('./components/ImageModal')
+    }
     return (
         <div className="App">
             <Header />
             <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
+            <ButtonModal onClick={() => { setShowModal(true) }} onMouseEnter={handleMouseEnter}>올림픽 사진 보기</ButtonModal>
             <SurveyChart />
             <Footer />
             <Suspense fallback={null}>
